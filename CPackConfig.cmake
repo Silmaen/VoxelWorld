a127a07a6a0c199e5a01d0206d5ae82e8331e651
Silmaen <genteur.slayer@laposte.net>
2024-05-22 20:38:07 +0200

    Working packaging

---
+++ CPackConfig.cmake
@@ -0,0 +1,74 @@
+#
+# Packaging configuration
+#
+if (${PRJPREFIX}_PACKAGE_ENGINE)
+    include(GNUInstallDirs)
+    # Create config file for FindPackage
+    foreach (config IN ITEMS Debug Release)
+        install(EXPORT OwlTargets${CMAKE_SYSTEM_NAME}${CMAKE_SYSTEM_PROCESSOR}-${config}
+                FILE OwlTargets${CMAKE_SYSTEM_NAME}.cmake
+                NAMESPACE Owl::
+                CONFIGURATIONS ${config}
+                DESTINATION cmake
+                COMPONENT Engine)
+    endforeach ()
+    # Create file for use of find_package
+    include(CMakePackageConfigHelpers)
+    configure_package_config_file(
+            ${CMAKE_CURRENT_LIST_DIR}/cmake/config/OwlEngineConfig.cmake.in
+            ${CMAKE_CURRENT_BINARY_DIR}/OwlEngineConfig.cmake
+            INSTALL_DESTINATION cmake)
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/OwlEngineConfig.cmake DESTINATION cmake)
+endif ()
+
+set(CPACK_PACKAGE_NAME "Owl")
+if (${PRJPREFIX}_PACKAGE_NAME) # to be defined in presets
+    set(CPACK_PACKAGE_NAME "${${PRJPREFIX}_PACKAGE_NAME}")
+endif ()
+set(CPACK_PACKAGE_VENDOR "${${PRJPREFIX}_AUTHOR}")
+set(CPACK_PACKAGE_VERSION_MAJOR ${CMAKE_PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${CMAKE_PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${CMAKE_PROJECT_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
+# Get the latest abbreviated commit hash of the working branch
+execute_process(
+        COMMAND git log -1 --format=%h
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE ${PRJPREFIX}_GIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+if (${PRJPREFIX}_PACK_TIME)
+    string(TIMESTAMP NOW "-%Y%m%d_%H%M")
+endif ()
+
+if (${PRJPREFIX}_DISTRIBUTION)
+    set(${PRJPREFIX}_OS_VERSION "${${PRJPREFIX}_DISTRIBUTION}")
+else ()
+
+    if (${PRJPREFIX}_PLATFORM_ANDROID)
+        set(${PRJPREFIX}_OS_VERSION "${CMAKE_SYSTEM_VERSION}")
+    elseif (${PRJPREFIX}_PLATFORM_IOS OR ${PRJPREFIX}_PLATFORM_MACOS)
+        set(${PRJPREFIX}_OS_VERSION "${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    endif ()
+endif ()
+
+if (${PRJPREFIX}_GLIBC_STR)
+    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}${NOW}-${${PRJPREFIX}_GIT_HASH}-${${PRJPREFIX}_PLATFORM_STR}-${${PRJPREFIX}_GLIBC_STR}-${${PRJPREFIX}_ARCH_STR}")
+else ()
+    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}${NOW}-${${PRJPREFIX}_GIT_HASH}-${${PRJPREFIX}_PLATFORM_STR}-${${PRJPREFIX}_ARCH_STR}")
+endif ()
+if (NOT ${PRJPREFIX}_BUILD_SHARED)
+    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}-static")
+else ()
+    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}-shared")
+endif ()
+
+message(STATUS "PACKAGING Expected package Name: ${CPACK_PACKAGE_FILE_NAME}")
+
+if (${PRJPREFIX}_PLATFORM_WINDOWS)
+    set(CPACK_GENERATOR "ZIP")
+else ()
+    set(CPACK_GENERATOR "TGZ")
+endif ()
+
+message(STATUS "Using ${CPACK_GENERATOR} Package generator")
---
+++ cmake/importSharedLibs.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from shutil import copy2
+from sys import stderr
+
+script_path = Path(__file__).resolve().parent
+root_path = script_path.parent
+
+
+def list_missing_so(binary: Path, has_missing: bool):
+    from subprocess import run, STDOUT, PIPE
+    try:
+        out = run(f"ldd {binary}", shell=True, stdout=PIPE, stderr=STDOUT)
+        if out.returncode != 0:
+            print(f"Error while searching dependencies: {out.returncode}\n{out.stdout}", file=stderr)
+            exit(1)
+        lines = out.stdout.decode().replace("\t", "").splitlines(keepends=False)
+        missing_so = []
+        if has_missing:
+            for line in lines:
+                if "not found" not in line:
+                    continue
+                missing_so.append(line.split()[0])
+        extern_so = []
+        for line in lines:
+            if ".edm" not in line:
+                continue
+            items = line.split()
+            if (binary.parent / items[0]).exists():
+                continue
+            extern_so.append([items[0], items[2]])
+        return missing_so, extern_so
+    except Exception as err:
+        print(f"Exception while searching dependencies: {err}", file=stderr)
+        exit(1)
+
+
+def list_available_so(paths: str):
+    so_list = []
+    try:
+        for path in [Path(p) for p in paths.split(";")]:
+            while "lib" in f"{path}" or "bin" in f"{path}" or "cmake" in f"{path}":
+                path = path.parent
+            so_list += list(path.rglob("*.so*"))
+    except Exception as err:
+        print(f"Error in inspecting available so: {err}")
+    return so_list
+
+
+def main():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("binary", type=str, help="The binary file to search for dependency")
+    parser.add_argument("lib_list", type=str, help="A string in the format of CMAKE_MODULE_PATH")
+    args = parser.parse_args()
+
+    binary_file = Path(args.binary).resolve()
+    if not binary_file.exists():
+        print(f"Error: file {binary_file} does not exists!", file=stderr)
+        exit(1)
+    binary_dir = binary_file.parent
+    lib_list = args.lib_list
+    has_missing = True
+    if lib_list in ["", None]:
+        has_missing = False
+
+    missing, extern = list_missing_so(binary_file, has_missing)
+    if len(missing) + len(extern) > 0:
+        if len(missing) > 0:
+            print("Initial missing list:")
+            for miss in missing:
+                print(f" -- {miss}")
+        if len(extern) > 0:
+            print("Initial missing list:")
+            for ext in extern:
+                print(f" -- {ext[0]} <- {ext[1]}")
+    else:
+        print("Nothing is missing!")
+        exit(0)
+
+    not_found = []
+    if len(missing) > 0:
+        available_so = list_available_so(lib_list)
+    else:
+        available_so = []
+    go_on = True
+    while go_on:
+        go_on = False
+        missing, extern = list_missing_so(binary_file, has_missing)
+        if len(missing) + len(extern) == 0:
+            break
+        for miss in missing:
+            if miss in not_found:
+                continue
+            found = False
+            for av in available_so:
+                if miss == av.name:
+                    try:
+                        copy2(av, binary_dir / miss)
+                    except Exception as err:
+                        print(f"ERROR while copy {av} to {binary_dir / miss} : {err}", file=stderr)
+                        exit(1)
+                    go_on = True
+                    found = True
+                    break
+            if not found:
+                not_found.append(miss)
+                print(f"Warning: dependency {miss}, not found.", file=stderr)
+        for ext in extern:
+            try:
+                copy2(ext[1], binary_dir / ext[0])
+            except Exception as err:
+                print(f"ERROR while copy {ext[1]} to {binary_dir / ext[0]} : {err}", file=stderr)
+                exit(1)
+    missing, extern = list_missing_so(binary_file, has_missing)
+    if len(missing) + len(extern) == 0:
+        print(f"Success: all missing dependencies have been solved")
+        exit(0)
+    else:
+        for miss in missing:
+            print(f"{miss} is still missing.")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
